38756faaa415e9b0da0fcd5acfc29d2c5119f43b
Matthew Solórzano <msolorzano768@gmail.com>
2020-02-29 11:24:50 -0500

    Create namegen.py
    I'm adding this file to the project to create names for test characters.
    just some fun!

---
+++ namegen.py
@@ -0,0 +1,26 @@
+from random import randint
+name_parts = ["me", "cha", "bra", "mo", "chi", "bra", "mi", "ha", "ra", "ma", "tha", "ka", "po"]
+
+def select_name_part(name_list):
+    x = randint(0, len(name_list) - 1)
+    return name_list[x]
+
+def create_name(length_of_name, name_list):
+    name = ''
+    for x in range(length_of_name):
+        name += select_name_part(name_list)
+    return name
+
+def create_name_list(listpop):
+    names_list = []
+    for x in range(listpop):
+        names_list.append((create_name(randint(2, 5), name_parts)).capitalize())
+    return names_list
+
+for names in create_name_list(20):
+    print(names)
+
+
+
+
+
