cbbf4df328d27d733133980548dc6ab23d0a50d2
Matthew Solórzano <msolorzano768@gmail.com>
2020-03-02 19:07:48 -0500

    Updated battle.py Created initial battle state

--- battle.py
+++ battle.py
@@ -5,23 +5,40 @@ import Items
 # This class will hold the logic for battles in the game.
 # Battles are free for all but each player can be assigned a team.
 # Last team standing wins.
-# There can be unlimited teams.
+# # There can be unlimited teams.
 
 class Battle:
-    def __init__(self, characters):
+    def __init__(self, characters, stage='default'):
         # this should be a list of dictionaries (keys = character and team)
         self.characters = characters
+        self.stage = stage
+        self.time = 0
 
     def start_battle(self):
-        # determine everyone's turn order and speed advantage
         # generate the timeline
-        pass
+        timeline = []
+        self.time = 0
+
+        # determine speed advantage
+        for characters in self.characters:
+            timeline = {'character': characters, 'pos': characters.get_spd_advantage()}
+            # create this function in player.py
+
     def take_turn(self):
+
+        # display options
+        # take action
+        # select target
+        # resolves effects
+
         # progress the timeline one step
-        pass
+        self.time += 1
+        return
+
     def end_battle(self):
         # generates loot for winning team (if player)
         # awards xp
+        # displays status screen
         pass
 
     def generate_loot(self):
